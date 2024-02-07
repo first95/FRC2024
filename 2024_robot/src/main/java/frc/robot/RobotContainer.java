72e06a789e5ddce2dc7ad2e433aabf2015dd7d4a
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2024-02-07 00:37:42 -0500

    remove unused import

--- 2024_robot/src/main/java/frc/robot/RobotContainer.java
+++ 2024_robot/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.ShooterConstants;
 //import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.drivebase.TeleopDrive;
@@ -13,7 +12,6 @@ import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.SwerveBase;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
