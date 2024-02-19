53b2067d551b93e59defe4b8aa21b0e4dca1373c
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2024-02-19 10:16:23 -0500

    accidentally removed command during merge

--- 2024_robot/src/main/java/frc/robot/RobotContainer.java
+++ 2024_robot/src/main/java/frc/robot/RobotContainer.java
@@ -25,9 +25,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -40,42 +43,65 @@ public class RobotContainer {
 
   private final CommandJoystick driveController = new CommandJoystick(OperatorConstants.driveControllerPort);
   private final CommandJoystick headingController = new CommandJoystick(OperatorConstants.headingControllerPort);
-  private final CommandXboxController operatorController = new CommandXboxController(OperatorConstants.operatorControllerPort);
+  private final CommandXboxController operatorController = new CommandXboxController(
+      OperatorConstants.operatorControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
 
     openRobotRel = new TeleopDrive(
-      drivebase,
-      () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63 : 0,
-      () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63 : 0,
-      () -> -headingController.getTwist() * 0.63, () -> false, true);
-    
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63
+            : 0,
+        () -> -headingController.getTwist() * 0.63, () -> false, true);
+
     closedRobotRel = new TeleopDrive(
-      drivebase,
-      () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63 : 0,
-      () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63 : 0,
-      () -> -headingController.getTwist() * 0.63, () -> false, false);
-    
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63
+            : 0,
+        () -> -headingController.getTwist() * 0.63, () -> false, false);
+
     openFieldRel = new TeleopDrive(
-      drivebase,
-      () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? driveController.getY() * 0.63 : 0,
-      () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? driveController.getX() * 0.63 : 0,
-      () -> -headingController.getTwist() * 0.63, () -> true, true);
-    
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? driveController.getY() * 0.63
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? driveController.getX() * 0.63
+            : 0,
+        () -> -headingController.getTwist() * 0.63, () -> true, true);
+
     closedFieldRel = new TeleopDrive(
-      drivebase,
-      () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63 : 0,
-      () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63 : 0,
-      () -> -headingController.getTwist() * 0.63, () -> true, false);
-    
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63
+            : 0,
+        () -> -headingController.getTwist() * 0.63, () -> true, false);
+
     absoluteDrive = new AbsoluteDrive(
-      drivebase,
-      () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63 : 0,
-      () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63 : 0,
-      () -> -headingController.getX(),
-      () -> -headingController.getY(), false);
-    
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband) ? -driveController.getY() * 0.63
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband) ? -driveController.getX() * 0.63
+            : 0,
+        () -> -headingController.getX(),
+        () -> -headingController.getY(), false);
+
+    NoteHandlerCommand noteManager = new NoteHandlerCommand(
+        shooter,
+        intake,
+        () -> (operatorController.getLeftTriggerAxis() - operatorController.getRightTriggerAxis()),
+        () -> operatorController.y().getAsBoolean(),
+        () -> operatorController.rightBumper().getAsBoolean(),
+        () -> operatorController.leftBumper().getAsBoolean());
+
+    shooter.setDefaultCommand(noteManager);
+
     drivebase.setDefaultCommand(closedFieldRel);
     // Configure the trigger bindings
     configureBindings();
@@ -89,12 +115,17 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
@@ -107,6 +138,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return null;//Autos.exampleAuto(m_exampleSubsystem);
+    return null;// Autos.exampleAuto(m_exampleSubsystem);
   }
 }
