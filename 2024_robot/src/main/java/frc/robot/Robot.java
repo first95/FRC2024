0c82e69a8de1ab143cd48e1e366b331645923615
Adam Johnson <adam.a.johnson@gmail.com>
2024-02-19 23:25:17 -0500

    Copied Version display code from Clarke to 2024_robot

--- 2024_robot/src/main/java/frc/robot/Robot.java
+++ 2024_robot/src/main/java/frc/robot/Robot.java
@@ -28,6 +28,23 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    // Grab the build computer, branchname, git commit ID and build timestamp from the Jar manifest
+    // and toss the on the smart dashboard
+    
+    String hostbranch = Robot.class.getPackage().getImplementationTitle();
+    if ( hostbranch != null && ! hostbranch.isEmpty()) {
+      SmartDashboard.putString("BuildHost-BranchName", hostbranch);
+    } else {
+      SmartDashboard.putString("BuildHost-BranchName","No JAR Manifest in simulation");
+    }
+    
+    String CommitIDtime = Robot.class.getPackage().getImplementationVersion();
+    if (CommitIDtime != null && ! CommitIDtime.isEmpty()) {
+      SmartDashboard.putString("GitCommitID-BuildTimestamp", CommitIDtime);
+    } else {
+      SmartDashboard.putString("GitCommitID-BuildTimestamp", "No JAR Manifest in simulation");
+    }
   }
 
   /**
