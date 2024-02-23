653215f9f8a4c98df153cb630b26115093a48717
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2024-02-23 00:06:59 -0500

    whaitespace and fixed trajectory imports

--- 2024_robot/src/main/java/frc/robot/RobotContainer.java
+++ 2024_robot/src/main/java/frc/robot/RobotContainer.java
@@ -52,174 +52,176 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-    private final Map<String, ChoreoTrajectory> trajMap;
-    
-        // The robot's subsystems and commands are defined here...
-    private final Intake intake = new Intake();
-    private final SwerveBase drivebase = new SwerveBase();
-    private final Shooter shooter = new Shooter();
-    private final TeleopDrive openRobotRel, closedRobotRel, openFieldRel, closedFieldRel;
-    private final AbsoluteDrive absoluteDrive;
+  private final Map<String, ChoreoTrajectory> trajMap;
 
-    private final CommandJoystick driveController = new CommandJoystick(OperatorConstants.driveControllerPort);
-    private final CommandJoystick headingController = new CommandJoystick(OperatorConstants.headingControllerPort);
-    private final CommandXboxController operatorController = new CommandXboxController(
-            OperatorConstants.operatorControllerPort);
+  // The robot's subsystems and commands are defined here...
+  private final Intake intake = new Intake();
+  private final SwerveBase drivebase = new SwerveBase();
+  private final Shooter shooter = new Shooter();
+  private final TeleopDrive openRobotRel, closedRobotRel, openFieldRel, closedFieldRel;
+  private final AbsoluteDrive absoluteDrive;
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
+  private final CommandJoystick driveController = new CommandJoystick(OperatorConstants.driveControllerPort);
+  private final CommandJoystick headingController = new CommandJoystick(OperatorConstants.headingControllerPort);
+  private final CommandXboxController operatorController = new CommandXboxController(
+      OperatorConstants.operatorControllerPort);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+
+    openRobotRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> false, true);
+
+    closedRobotRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> false, false);
+
+    openFieldRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> true, true);
+
+    closedFieldRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> true, false);
+
+    absoluteDrive = new AbsoluteDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getX(),
+        () -> -headingController.getY(), false);
+
+    NoteHandlerCommand noteManager = new NoteHandlerCommand(
+        shooter,
+        intake,
+        () -> (operatorController.getLeftTriggerAxis() - operatorController.getRightTriggerAxis()),
+        () -> operatorController.y().getAsBoolean(),
+        () -> operatorController.leftBumper().getAsBoolean(),
+        () -> operatorController.rightBumper().getAsBoolean(),
+        () -> operatorController.a().getAsBoolean());
+
+    shooter.setDefaultCommand(noteManager);
+
+    drivebase.setDefaultCommand(absoluteDrive);
+    // Configure the trigger bindings
+    configureBindings();
+
+    trajMap = loadTrajectories();
+
+    SmartDashboard.putData("setGains", new InstantCommand(drivebase::setVelocityModuleGains));
+
+    SmartDashboard.putNumber("KV", Drivebase.KV);
+    SmartDashboard.putNumber("KA", Drivebase.KA);
+    SmartDashboard.putNumber("KP", 0);
+    SmartDashboard.putNumber("KI", 0);
+    SmartDashboard.putNumber("KD", 0);
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    /*
+     * operatorController.y().whileTrue(shooter.sysIdQuasiShoulder(SysIdRoutine.
+     * Direction.kForward));
+     * operatorController.b().whileTrue(shooter.sysIdQuasiShoulder(SysIdRoutine.
+     * Direction.kReverse));
+     * operatorController.a().whileTrue(shooter.sysIdDynShoulder(SysIdRoutine.
+     * Direction.kForward));
+     * operatorController.x().whileTrue(shooter.sysIdDynShoulder(SysIdRoutine.
+     * Direction.kReverse));
      */
-    public RobotContainer() {
+    driveController.button(1).whileTrue(new AutoShoot(drivebase));
+    driveController.button(8).onTrue(new InstantCommand(drivebase::clearOdometrySeed).ignoringDisable(true));
+    headingController.button(1).whileTrue(new AutoAmp(drivebase)).onFalse(new InstantCommand(() -> {
+      SmartDashboard.putBoolean(Auton.AUTO_AMP_SCORE_KEY, false);
+      SmartDashboard.putBoolean(Auton.AUTO_AMP_ALIGN_KEY, false);
+    }));
+    // headingController.button(1).whileTrue(new AlignToPose("ScoreAmp",
+    // drivebase));
+  }
 
-        openRobotRel = new TeleopDrive(
-                drivebase,
-                () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getY()
-                        : 0,
-                () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getX()
-                        : 0,
-                () -> -headingController.getTwist(), () -> false, true);
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    // return Autos.twoNoteCenter(drivebase, intake, trajMap);
+    return null;
+  }
 
-        closedRobotRel = new TeleopDrive(
-                drivebase,
-                () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getY()
-                        : 0,
-                () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getX()
-                        : 0,
-                () -> -headingController.getTwist(), () -> false, false);
+  public void sendAlliance() {
+    drivebase.setAlliance(DriverStation.getAlliance().get());
+  }
 
-        openFieldRel = new TeleopDrive(
-                drivebase,
-                () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
-                        ? driveController.getY()
-                        : 0,
-                () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
-                        ? driveController.getX()
-                        : 0,
-                () -> -headingController.getTwist(), () -> true, true);
-
-        closedFieldRel = new TeleopDrive(
-                drivebase,
-                () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getY()
-                        : 0,
-                () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getX()
-                        : 0,
-                () -> -headingController.getTwist(), () -> true, false);
-
-        absoluteDrive = new AbsoluteDrive(
-                drivebase,
-                () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getY()
-                        : 0,
-                () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
-                        ? -driveController.getX()
-                        : 0,
-                () -> -headingController.getX(),
-                () -> -headingController.getY(), false);
-
-        NoteHandlerCommand noteManager = new NoteHandlerCommand(
-                shooter,
-                intake,
-                () -> (operatorController.getLeftTriggerAxis() - operatorController.getRightTriggerAxis()),
-                () -> operatorController.y().getAsBoolean(),
-                () -> operatorController.leftBumper().getAsBoolean(),
-                () -> operatorController.rightBumper().getAsBoolean(),
-                () -> operatorController.a().getAsBoolean());
-
-        shooter.setDefaultCommand(noteManager);
-
-        drivebase.setDefaultCommand(absoluteDrive);
-        // Configure the trigger bindings
-        configureBindings();
-        
-        //trajMap = loadTrajectories();
-        trajMap = null;
-
-        SmartDashboard.putData("setGains", new InstantCommand(drivebase::setVelocityModuleGains));
-
-        SmartDashboard.putNumber("KV", Drivebase.KV);
-        SmartDashboard.putNumber("KA", Drivebase.KA);
-        SmartDashboard.putNumber("KP", 0);
-        SmartDashboard.putNumber("KI", 0);
-        SmartDashboard.putNumber("KD", 0);
+  private Map<String, ChoreoTrajectory> loadTrajectories() {
+    Set<String> trajNames;
+    try {
+      if (Robot.isReal()) {
+        trajNames = listFilesUsingFilesList("/deploy/choreo");
+      } else {
+        trajNames = listFilesUsingFilesList("src/main/deploy/choreo");
+      }
+    } catch (IOException e) {
+      DriverStation.reportError("Invalid Directory! Trajectories failed to load!", true);
+      return null;
     }
+    return trajNames.stream().collect(Collectors.toMap(
+        entry -> entry,
+        entry -> Choreo.getTrajectory(entry.replace(".traj", ""))));
+  }
 
-    /**
-     * Use this method to define your trigger->command mappings. Triggers can be
-     * created via the
-     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-     * an arbitrary
-     * predicate, or via the named factories in {@link
-     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-     * {@link
-     * CommandXboxController
-     * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-     * PS4} controllers or
-     * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-     * joysticks}.
-     */
-    private void configureBindings() {
-        /*
-         * operatorController.y().whileTrue(shooter.sysIdQuasiShoulder(SysIdRoutine.
-         * Direction.kForward));
-         * operatorController.b().whileTrue(shooter.sysIdQuasiShoulder(SysIdRoutine.
-         * Direction.kReverse));
-         * operatorController.a().whileTrue(shooter.sysIdDynShoulder(SysIdRoutine.
-         * Direction.kForward));
-         * operatorController.x().whileTrue(shooter.sysIdDynShoulder(SysIdRoutine.
-         * Direction.kReverse));
-         */
-        driveController.button(1).whileTrue(new AutoShoot(drivebase));
-        driveController.button(8).onTrue(new InstantCommand(drivebase::clearOdometrySeed).ignoringDisable(true));
-        headingController.button(1).whileTrue(new AutoAmp(drivebase)).onFalse(new InstantCommand(() -> {
-                SmartDashboard.putBoolean(Auton.AUTO_AMP_SCORE_KEY, false);
-                SmartDashboard.putBoolean(Auton.AUTO_AMP_ALIGN_KEY, false);
-        }
-        ));
-        //headingController.button(1).whileTrue(new AlignToPose("ScoreAmp", drivebase));
+  public Set<String> listFilesUsingFilesList(String dir) throws IOException {
+    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+      return stream
+          .filter(file -> !Files.isDirectory(file))
+          .map(Path::getFileName)
+          .map(Path::toString)
+          .collect(Collectors.toSet());
     }
-
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // An example command will be run in autonomous
-        //return Autos.twoNoteCenter(drivebase, intake, trajMap);
-        return null;
-    }
-
-    public void sendAlliance() {
-        drivebase.setAlliance(DriverStation.getAlliance().get());
-    }
-
-    private Map<String, ChoreoTrajectory> loadTrajectories() {
-        Set<String> trajNames;
-        try {
-            trajNames = listFilesUsingFilesList("/deploy/choreo");
-        } catch (IOException e) {
-            DriverStation.reportError("Invalid Directory! Trajectories failed to load!", true);
-            return null;
-        }
-        return trajNames.stream().collect(Collectors.toMap(
-            entry -> entry,
-            entry -> Choreo.getTrajectory(entry.split("\\.")[0])
-        ));
-    }
-
-    public Set<String> listFilesUsingFilesList(String dir) throws IOException {
-        try (Stream<Path> stream = Files.list(Paths.get(dir))) {
-            return stream
-                    .filter(file -> !Files.isDirectory(file))
-                    .map(Path::getFileName)
-                    .map(Path::toString)
-                    .collect(Collectors.toSet());
-        }
-    }
+  }
 }
