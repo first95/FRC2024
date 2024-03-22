5371fe8eea1a686c7a41cc86c6e54cedf8cee670
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2024-03-21 20:24:38 -0400

    revert odom std dev for tele practice

--- Judith/src/main/java/frc/robot/Constants.java
+++ Judith/src/main/java/frc/robot/Constants.java
@@ -213,7 +213,7 @@ public final class Constants {
         public static final double ANGULAR_ERROR_TOLERANCE = Math.toRadians(5);
         public static final int LOOP_CYCLES_BEFORE_RESET = 20;
 
-        public static final double ODOMETRY_TRANSLATIONAL_STD_DEV = 0.01; // Meters and radians
+        public static final double ODOMETRY_TRANSLATIONAL_STD_DEV = 0.003; // Meters and radians
         public static final double ODOMETRY_ANGULAR_STD_DEV = 0.0002;
 
         public static final double XY_STD_DEV_COEFFICIENT = 0.01;
